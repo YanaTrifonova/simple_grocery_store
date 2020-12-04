7929d09a9d186addd3a8f68bf78e5f40781182f9
YanaTrifonova <trifonova.yana.prof@gmail.com>
2020-12-04 15:10:34 +0100

    step 1 | added response for products

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 
 const categoryRouter = require("./routers/category");
+const productRouter = require("./routers/product");
 
 const PORT = 4000;
 
@@ -12,5 +13,6 @@ app.use(express.json());
 
 // Routers
 app.use(categoryRouter);
+app.use(productRouter);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}...`));
---
+++ routers/product.js
@@ -0,0 +1,33 @@
+const {Router} = require("express");
+const router = new Router();
+
+const Product = require('../models').product;
+
+router.get('/products', async (req, res, next) => {
+    try {
+        const products = await Product.findAll();
+
+        if(!products) return res.status(404).send("no products were found")
+
+        res.json(products);
+
+    } catch (e) {
+        next(e);
+    }
+})
+
+router.get('/products/:id', async (req, res, next) => {
+    try {
+        const productId = parseInt(req.params.id);
+        const product = await Product.findByPk(productId);
+
+        if(!product) return res.status(404).send("this product id not found");
+
+        res.json(product);
+
+    } catch (e) {
+        next(e);
+    }
+})
+
+module.exports = router;
